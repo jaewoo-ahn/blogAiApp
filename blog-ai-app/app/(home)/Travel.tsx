f7bf8705eadd9a2dd0e4b5182e7ff354fc0f9b8b
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-05-01 20:54:42 +0900

    travel header

--- blog-ai-app/app/(home)/Travel.tsx
+++ blog-ai-app/app/(home)/Travel.tsx
@@ -3,7 +3,17 @@ import React from "react";
 type Props = {};
 
 const Travel = (props: Props) => {
-  return <div>Travel</div>;
+  return (
+    <section className="mt-10">
+      <hr className="border-1" />
+      <div className="flex items-center gap-3 my-8">
+        <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
+          TRAVEL
+        </h4>
+        <p className="font-bold text-2xl">New Travel Experience</p>
+      </div>
+    </section>
+  );
 };
 
 export default Travel;
